ffbcbc66932afae2454b1b968319473be99949b7
INP150 <hydra.elit@gmail.com>
2026-04-03 14:59:54 +0200

    Added command for version check

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import discord
+import subprocess
 from discord.ext import commands
 from discord import app_commands
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
 from const import EMOJI_MAP
 from message_helpers import gather_message_parts
@@ -16,12 +17,15 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
+    """Booting the bot"""
     await bot.tree.sync()
     print(f"{bot.user} is online!")
 
+
 @bot.event
-async def on_message(msg):
+async def on_message(msg) -> None:
+    """Checks all messages from users and add reactions if the text inlcudes defined words"""
     if msg.author.bot: # ignore messages from bots (including self) 
         return
 
@@ -42,17 +46,33 @@ async def on_message(msg):
         add_reactions_to_message(msg, pending_reactions)
     await bot.process_commands(msg)
 
+
 @bot.tree.command(name="ping", description="Check the bot's latency")
-async def ping(interaction: discord.Interaction):
+async def ping(interaction: discord.Interaction) -> None:
+    """Command to check the ping of the bot"""
     latency = bot.latency * 1000  # Convert to milliseconds
     await interaction.response.send_message(f"Pong! Latency: {latency:.2f} ms")
 
+
 @bot.tree.command(name="greet", description="Greet the user")
-async def greet(interaction: discord.Interaction):
+async def greet(interaction: discord.Interaction) -> None:
+    """Simple command to greet the user"""
     await interaction.response.send_message(f"Hello, {interaction.user.mention}!")
 
 
-def add_reactions_to_message(msg, emojis):
+@bot.tree.command(name="version", description="Get the bot's version")
+async def version(interaction: discord.Interaction) -> None:
+    """Command that shows on which commit the bot runs right now"""
+    version = get_latest_commit_hash()
+    await interaction.response.send_message(f"{version}")
+
+
+def get_latest_commit_hash() -> str:
+    """Returns the short version of the latest git commit hash"""
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+
+
+def add_reactions_to_message(msg, emojis) -> None:
     """Add a list of emojis as reactions to a message."""
     for emoji in emojis:
         try:
